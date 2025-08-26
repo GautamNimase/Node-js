1bcdbf022acc2081cdf0cfc26088d8dd45d64ee1
Gautam <gautamnimase007@gmail.com>
2025-08-26 14:46:22 +0530

    node

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello, World!")
+const fs=require('node:fs')
+
+// fs.writeFileSync('copy.text','this is new file')
+// fs.writeFileSync('copy.text','this is another file')
+// fs.appendFileSync('copy.text','\n hello my name is gautam')
+
+// console.log((fs.readFileSync('copy.text','utf-8')));
+// fs.mkdirSync('class/xyz/a',{recursive:true})
+fs.rmdirSync('class')
+
+
