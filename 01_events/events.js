cb1f903c42433a2fcb4240b44f3501bbd71466c2
Gautam <gautamnimase007@gmail.com>
2025-09-01 12:42:50 +0530

    events

---
+++ 01_events/events.js
@@ -0,0 +1,16 @@
+const EventEmitter=require("events");
+const eventEmitter=new EventEmitter();
+
+eventEmitter.on('greet',(username)=>{
+    console.log(`hello ${username} and welcome to event in node.js`);
+});
+
+eventEmitter.once('massage',()=>{
+    console.log("hey this will be print only once ")
+})
+
+eventEmitter.emit('greet','gautam');
+eventEmitter.emit('greeting');
+eventEmitter.emit('massage')
+eventEmitter.emit('massage')
+eventEmitter.emit('greet','champa');
