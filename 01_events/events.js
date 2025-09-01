d0297746ea1210a517807514d16be7f17e0420a8
Gautam <gautamnimase007@gmail.com>
2025-09-01 19:42:24 +0530

    error events

--- 01_events/events.js
+++ 01_events/events.js
@@ -1,16 +1,22 @@
 const EventEmitter=require("events");
 const eventEmitter=new EventEmitter();
 
-eventEmitter.on('greet',(username)=>{
-    console.log(`hello ${username} and welcome to event in node.js`);
+// eventEmitter.on('greet',(username)=>{
+//     console.log(`hello ${username} and welcome to event in node.js`);
+// });
+
+// eventEmitter.once('massage',()=>{
+//     console.log("hey this will be print only once ")
+// })
+
+// eventEmitter.emit('greet','gautam');
+// eventEmitter.emit('greeting');
+// eventEmitter.emit('massage')
+// eventEmitter.emit('massage')
+// eventEmitter.emit('greet','champa');
+
+eventEmitter.on("error",(err)=>{
+    console.error(`Error Occurred: ${err.message}`);
 });
 
-eventEmitter.once('massage',()=>{
-    console.log("hey this will be print only once ")
-})
-
-eventEmitter.emit('greet','gautam');
-eventEmitter.emit('greeting');
-eventEmitter.emit('massage')
-eventEmitter.emit('massage')
-eventEmitter.emit('greet','champa');
+eventEmitter.emit("error",new Error("Something Went Wrong "))
