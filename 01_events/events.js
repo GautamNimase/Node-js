e84e6a8fe56f2d1bc030e333560e264776ffb037
Gautam <gautamnimase007@gmail.com>
2025-09-11 22:14:38 +0530

    js

--- 01_events/events.js
+++ 01_events/events.js
@@ -1,22 +1,14 @@
 const EventEmitter=require("events");
+
 const eventEmitter=new EventEmitter();
 
-// eventEmitter.on('greet',(username)=>{
-//     console.log(`hello ${username} and welcome to event in node.js`);
-// });
-
-// eventEmitter.once('massage',()=>{
-//     console.log("hey this will be print only once ")
-// })
-
-// eventEmitter.emit('greet','gautam');
-// eventEmitter.emit('greeting');
-// eventEmitter.emit('massage')
-// eventEmitter.emit('massage')
-// eventEmitter.emit('greet','champa');
-
-eventEmitter.on("error",(err)=>{
-    console.error(`Error Occurred: ${err.message}`);
+eventEmitter.on('test',(username)=>{
+    console.log(`hello ${username} and welcome to node js`);
+});
+eventEmitter.on('test',(score)=>{
+    console.log(`your score is 45`);
 });
 
-eventEmitter.emit("error",new Error("Something Went Wrong "))
+eventEmitter.emit('test',"gautam");
+
+console.log(eventEmitter.listenerCount('test'))
